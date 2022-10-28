8bdaa1d510db77e794daf61570d5588ddec07572
Heberson Silva <ti.heberson@gmail.com>
2022-10-28 10:56:22 -0300

    Update 2-trabalhandoComVariaveis.js

--- JavaScript 1/2-trabalhandoComVariaveis.js
+++ JavaScript 1/2-trabalhandoComVariaveis.js
@@ -4,7 +4,31 @@ const idade = 36;
 const ano = 2022;
 
 
-console.log(7+7, 7-3 , 7*1 , 7/7); 
-console.log(7-3);
-console.log(7*1);
-console.log(7/7);
+console.log("Operações Aritiméticas");
+
+console.log(2+2, 3-1 , 3*1 , 2/2); 
+console.log(4-3);
+console.log(3*1);
+console.log(2/2);
+
+console.log(2+2);
+console.log(10 + 8 *2);
+
+console.log(2+2);
+console.log(10 + 8 *2);
+console.log((10 + 8) * 2);
+
+console.log("ano" + 2020);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+console.log("10"/ "2");
+console.log("7" / "2") 
+console.log(6.5);
+
+console.log("Trabalhando com atribuição de variáveis");
+//const idade = 36;
+let nome = "Heberson";
+const sobrenome = "Silva"; 
+
+console.log(nome, sobrenome);
