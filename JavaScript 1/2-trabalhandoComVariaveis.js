0af731e7296a71f39786f8d8d75216e781c0770e
Heberson Silva <ti.heberson@gmail.com>
2022-10-28 10:34:16 -0300

    Create 2-trabalhandoComVariaveis.js

---
+++ JavaScript 1/2-trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com variaveis em JS");
+
+const idade = 36;
+const ano = 2022;
+
+
+console.log(7+7, 7-3 , 7*1 , 7/7); 
+console.log(7-3);
+console.log(7*1);
+console.log(7/7);
